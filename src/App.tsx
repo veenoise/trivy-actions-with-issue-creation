7bbcc7e8a93cd7bb3aa9b812b08a0ba9e7e62649
William Chua <william.eduard123@gmail.com>
2026-03-19 17:29:50 +0800

    feat: testing openreview

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <p className="read-the-docs">
-        Testing
+        Testing openreview
       </p>
     </>
   )
